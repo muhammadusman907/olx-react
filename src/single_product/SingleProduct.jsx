6f52c485385df7290543850de26847c3fe824693
muhammadusman907 <muhammadusman90712@gmail.com>
2024-02-01 09:25:48 +0500

    chat page add

--- src/single_product/SingleProduct.jsx
+++ src/single_product/SingleProduct.jsx
@@ -3,12 +3,15 @@ import "./single_product.css";
 // import { query } from "firebase/firestore"
 import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import  { Navigate } from "react-router-dom"
 import { doc, db, getDoc } from "../config/firbase.js";
-import LOGO from "../Images/PROFILE.png"
+import LOGO from "../Images/PROFILE.png";
 
 export const SingleProduct = () => {
   const [searchParams] = useSearchParams();
   const [singleProductItem, setSingleProductItem] = useState({});
+  const [chatPageOpen, setChatPageOpen] = useState(false);
+  const [userinfo , setUserinfo] = useState({})
   //   ===================================================
   // =================== get single product with fire base
   // =====================================================
@@ -21,10 +24,17 @@ export const SingleProduct = () => {
       //   setSingleProductItem(docSnap.data());
       console.log("Document data:", docSnap.data());
       setSingleProductItem(docSnap.data());
+      setUserinfo(docSnap.data().userName)
     } else {
       // docSnap.data() will be undefined in this case
       console.log("No such document!");
     }
+  };
+  // =================================
+  //======================== chat page
+  // =================================
+  const chatPage = () => {
+    setChatPageOpen(true);
   };
 
   useEffect(() => {
@@ -33,6 +43,9 @@ export const SingleProduct = () => {
 
   return (
     <>
+      {chatPageOpen ? 
+        <Navigate to={`chat?chat=${searchParams.get("id")}`} />
+      :
       <Row className="ms-10 mt-12 me-10">
         <Col span={16}>
           <div className="image-card">
@@ -52,18 +65,48 @@ export const SingleProduct = () => {
             <h5 className="font-bold">{singleProductItem.productName}</h5>
           </Card>
           <Card className="my-shadow rounded-none mt-2">
-            <h1>Description</h1>
+            <h3 className="font-bold">Description</h3>
             <p>{singleProductItem.description}</p>
           </Card>
         </Col>
         <Col span={8}>
           <Card className="my-shadow ms-4">
-            <div className="w-[60px] h-[60px] rounded-full border-2">
-               <img src={LOGO}  alt="" />
-            </div>
+            <Row align="middle">
+              <Col span={5}>
+                <div className="w-[60px] h-[60px] rounded-full border-2">
+                  <img src={LOGO} alt="" />
+                </div>
+              </Col>
+              <Col span={16}>
+                <span className="ps-2 font-bold cursor-pointer">
+                 
+                  {userinfo.email}
+                  {/* {console.log("username" , singleProductItem.userName.username)} */}
+                </span>
+                <div className="ps-2 font-bold cursor-pointer">
+                  see profile{">"}{" "}
+                </div>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col span={24}>
+                <Button
+                  type="primary"
+                  className="bg-blue-500 w-[100%] rounded-2 mt-3"
+                >
+                  Show Phone Number
+                </Button>
+              </Col>
+              <Col span={24}>
+                <Button className="w-[100%] rounded-2 mt-3" onClick={chatPage}>
+                  Chat
+                </Button>
+              </Col>
+            </Row>
           </Card>
         </Col>
-      </Row>
+      </Row>}
     </>
   );
 };
